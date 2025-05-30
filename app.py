e3909bf9ac6337620401e5fd42318788ac5443a1
MOFILFRANGA-20 <frangamofil@gmail.com>
2025-05-30 11:08:20 +0530

    Updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import uuid
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/images'
-POPPLER_PATH = r'C:\poppler-24.08.0\Library\bin'  # ← replace this with your actual path
+POPPLER_PATH = r'C:\poppler-24.08.0\Library\bin'  
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
@@ -20,7 +20,6 @@ def convert():
         pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{file_id}.pdf")
         pdf_file.save(pdf_path)
 
-        # Convert all pages of the PDF to images
         images = convert_from_path(pdf_path, poppler_path=POPPLER_PATH)
         image_urls = []
 
@@ -30,7 +29,7 @@ def convert():
             image.save(image_path, 'PNG')
             image_urls.append(f"/{image_path}")
 
-        os.remove(pdf_path)  # Optional: delete original PDF after conversion
+        os.remove(pdf_path)  
         return render_template('result.html', image_urls=image_urls)
 
     return redirect('/')
